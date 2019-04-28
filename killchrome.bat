0383ea8a6ef6857bb92bd720533562d3b296baa9
kenvenner <40737866+kenvenner@users.noreply.github.com>
2019-04-28 09:48:07 -0700

    Add files via upload

--- killchrome.bat
+++ killchrome.bat
@@ -1,2 +1,5 @@
+rem this kills all chrome and selenium processes
+rem used when a program does and does not clean up after itself
 TASKKILL /IM chrome.exe /F
 TASKKILL /IM chromedriver.exe /f
+rem eof
--- wineselenium.bat
+++ wineselenium.bat
@@ -9,37 +9,44 @@ sendgmail subject_adder=" - wineselenium STARTED"
 REM
 REM
 REM Messaging
-echo %date% %time% wine.py use selenium to read in new data >> wineselenium.log
+echo %date% %time% wineselenium.py use selenium to read in new data >> wineselenium.log
 REM
-python wine.py >> wineselenium.log 2>>wineselenium-err.log
+python wineselenium.py >> wineselenium.log 2>>wineselenium-err.log
 REM 
 REM check to see if we error'd out
 if errorlevel 1 (
    TASKKILL /IM chrome.exe /F
    TASKKILL /IM chromedriver.exe /f
-   echo %date% %time% wine.py run again to do failure 1 >> wineselenium.log
+   echo %date% %time% wineselenium.py run again to do failure 1 >> wineselenium.log
    sendgmail subject_adder=" - wineselenium RESTARTED 1" 
-   python wine.py >> wineselenium.log 2>>wineselenium-err.log
+   python wineselenium.py >> wineselenium.log 2>>wineselenium-err.log
 )
 if errorlevel 1 (
    TASKKILL /IM chrome.exe /F
    TASKKILL /IM chromedriver.exe /f
-   echo %date% %time% wine.py run again to do failure 2 >> wineselenium.log
+   echo %date% %time% wineselenium.py run again to do failure 2 >> wineselenium.log
    sendgmail subject_adder=" - wineselenium RESTARTED 2"
-   python wine.py >> wineselenium.log 2>>wineselenium-err.log
+   python wineselenium.py >> wineselenium.log 2>>wineselenium-err.log
 )
 if errorlevel 1 (
    TASKKILL /IM chrome.exe /F
    TASKKILL /IM chromedriver.exe /f
-   echo %date% %time% wine.py run again to do failure 3 >> wineselenium.log
+   echo %date% %time% wineselenium.py run again to do failure 3 >> wineselenium.log
    sendgmail subject_adder=" - wineselenium RESTARTED 3"
-   python wine.py >> wineselenium.log 2>>wineselenium-err.log
+   python wineselenium.py >> wineselenium.log 2>>wineselenium-err.log
 )
 if errorlevel 1 (
    TASKKILL /IM chrome.exe /F
    TASKKILL /IM chromedriver.exe /f
-   echo %date% %time% wine.py not run again to do failure 4 >> wineselenium.log
+   echo %date% %time% wineselenium.py not run again to do failure 4 >> wineselenium.log
    sendgmail subject_adder=" - wineselenium FAILED and not restarted"
+   echo %date% %time% wineselenium.py TERMINATE this program >> wineselenium.log
+   exit 1
+)
+REM notify if there is issue processing
+IF EXIST fail*.html (
+   echo %date% %time% wineselenium.py errors in HTML files >> wineselenium.log
+   sendgmail subject_adder=" - wineselenium had ERRORS please review log file and HTML files"
 )
 REM
 REM Messaging
